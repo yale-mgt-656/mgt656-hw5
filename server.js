5264737189c6b71e76aff449c14023b99d56461a
Tiago Cruz <onisciente@gmail.com>
2016-10-04 02:20:43 +0000

    Changing position of GET to above app.listen

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/eager-spider', function(request, response){
+    response.send('Ces moi!');
+});
+
 app.listen(process.env.PORT || 4000);
