b7a8cb0d2cd14482351ddef29e9119b7141c2ee8
Krystyna Taheri <krystyna.taheri@yale.edu>
2016-10-06 14:24:00 +0000

    nickname again

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello Wild-Heron!');
+    response.send('Hello MGT 660!');
 });
-app.get('/wild-heron', function(request, response){
+app.get('/nickname', function(request, response){
     response.send('Hello Wild-Heron!');
 });
 
