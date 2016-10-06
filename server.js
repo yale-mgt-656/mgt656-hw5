b27526b56c8aed612b6350c25304d29689657933
Krystyna Taheri <krystyna.taheri@yale.edu>
2016-10-06 16:35:59 +0000

    trial 3

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT 660!');
+    response.send('Hello MGT 660!!!');
 });
 app.get('/nickname', function(request, response){
-    response.send('Hello Wild-Heron!');
+    response.send('Hello Wild-Heron!!!');
 });
 
 app.listen(process.env.PORT || 4000);
